5cd3f81468730a46ace6f8d1b96138203043565d
Cristofer Sanz Blasco <cristofercri@gmail.com>
2014-05-29 21:24:23 +0200

    Maquetadas vistas de busqueda con HAML. Maquetado el footer y header como partials.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Walko::Application.routes.draw do
+
+  get "busqueda/new"
+  get "busqueda/index"
+
   devise_for :usuarios
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'busqueda#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

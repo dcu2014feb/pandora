911829d6896ee35d995c2f1c025e979b821d5bca
Cristofer Sanz Blasco <cristofercri@gmail.com>
2014-06-03 04:55:59 +0200

    Primera iteración de refactorización de población.

--- app/controllers/poblacion_controller.rb
+++ app/controllers/poblacion_controller.rb
@@ -1,20 +1,5 @@
 class PoblacionController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
   def show
     @poblacion = Poblacion.find(params[:id])
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
   end
 end
